a1637e5848d3a0a1779ecb68839e9e9c91d78dc5
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-16 10:43:45 +0500

    Header va Footer Qilindi

---
+++ shop/src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#App" class="brand-logo">
+          React Shop
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="#App">REPO</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
