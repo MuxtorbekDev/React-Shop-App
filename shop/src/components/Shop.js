eb030c50cc81378e7b388c973191773397803cc5
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-16 12:45:43 +0500

    Content qismi qo'shildi

---
+++ shop/src/components/Shop.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { API_URL, API_KEY } from "../config";
+import Loader from "./Loader";
+import GoodList from "./GoodList";
+
+export default function Shop() {
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.featured && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container content ">
+      {loading ? <Loader /> : <GoodList goods={goods} />}
+    </div>
+  );
+}
