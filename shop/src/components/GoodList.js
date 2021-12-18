93841a167f7a59bf6b92bcba408365662d45004d
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-18 11:40:21 +0500

    Buyurtma berish funcsiyasi qo'shildi

--- shop/src/components/GoodList.js
+++ shop/src/components/GoodList.js
@@ -1,7 +1,7 @@
 import GoodItem from "./GoodItem";
 
 export default function GoodList(props) {
-  const { goods = [] } = props;
+  const { goods = [], addToBasket } = props;
 
   if (!goods.length) {
     return <h3>Nothng here</h3>;
@@ -10,7 +10,7 @@ export default function GoodList(props) {
   return (
     <div className="goods">
       {goods.map((item) => (
-        <GoodItem key={item.id} {...item} />
+        <GoodItem key={item.id} {...item} addToBasket={addToBasket} />
       ))}
     </div>
   );
--- shop/src/components/Shop.js
+++ shop/src/components/Shop.js
@@ -7,6 +7,30 @@ import Cart from "./Cart";
 export default function Shop() {
   const [goods, setGoods] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
+
+  const addToBasket = (item) => {
+    const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
+    if (itemIndex < 0) {
+      const newItem = {
+        ...item,
+        quantity: 1,
+      };
+      setOrder([...order, newItem]);
+    } else {
+      const newOrder = order.map((orderItem, index) => {
+        if (index === itemIndex) {
+          return {
+            ...orderItem,
+            quantity: orderItem.quantity + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+      setOrder(newOrder);
+    }
+  };
 
   useEffect(() => {
     fetch(API_URL, {
@@ -23,8 +47,12 @@ export default function Shop() {
 
   return (
     <div className="container content ">
-      <Cart quantity={goods.length} />
-      {loading ? <Loader /> : <GoodList goods={goods} />}
+      <Cart quantity={order.length} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <GoodList goods={goods} addToBasket={addToBasket} />
+      )}
     </div>
   );
 }
